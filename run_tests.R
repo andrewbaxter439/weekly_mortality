3964a514d322607dd51710c269aa33d4d0b6b103
Andrew Baxter <andrewbaxter439@gmail.com>
2020-05-12 15:37:57 +0100

    re-run tests

--- run_tests.R
+++ run_tests.R
@@ -2,6 +2,6 @@ library(testthat)
 library(shinytest)
 
 test_that("Application works", {
-  expect_equal(1, 1)
-  # expect_pass(testApp(".", compareImages = FALSE))
+  # expect_equal(1, 1)
+  expect_pass(testApp(".", compareImages = FALSE))
 })
